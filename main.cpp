e9fadbe76f5d91571faa3a1c08d2c0e81982ff5b
PezzuttiFra <103060103+PezzuttiFra@users.noreply.github.com>
2022-04-06 11:57:27 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//dichiarazione
+    struct anagrafiche{
+        int idPersona;
+        string Cognome;
+        string Nome;
+        string Indirizzo;
+        string Localita;
+        string Sigla_provincia;
+        string Telefono;
+    };
+
+    anagrafiche arr[100];
+
+int main()
+{
+    return 0;
+}
+
