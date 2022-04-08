1f0279d5c85403b537ade2b5b5000319f828b927
Pilosio <103057112+Pilosio@users.noreply.github.com>
2022-04-08 08:39:05 +0200

    Add files via upload

---
+++ LetturaFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+void LetturaFile()
+{
+    ifstream Inputfile;
+    string stringa;
+    Inputfile.open("Progetto realta il testo.txt");
+    if (Inputfile != 0)
+    {
+        cout <<"ok il file si sta aprendo \n";
+        while(!Inputfile.eof())
+        {
+        	getline (Inputfile,stringa, ';');
+        	cout <<"  ";
+            cout << stringa;
+        }
+    }
+
+
+}
+
+int main()
+{
+	LetturaFile();
+  
+    return 0;
+}
