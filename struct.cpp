36186054b3f0408b584f87ba3c21230b16faf858
PezzuttiFra <103060103+PezzuttiFra@users.noreply.github.com>
2022-04-13 11:17:01 +0200

    Add files via upload

--- struct.cpp
+++ struct.cpp
@@ -1,11 +1,16 @@
-struct IdPersona{
-	
-	int Id;
-	string Cognome;
-	string Nome;
-	string Via;
-	string Località;
-	string Provincia;
-	int Telefono;
-	
-};
+#include <iostream>
+#include <string>
+
+using namespace std;
+//dichiarazione
+    struct anagrafiche{
+        int idPersona;
+        string Cognome;
+        string Nome;
+        string Indirizzo;
+        string Localita;
+        string Sigla_provincia;
+        string Telefono;
+    };
+
+    anagrafiche arr[100];
