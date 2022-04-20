14ecf4eec6335e4f05aa64c883b8121759467a4b
PezzuttiFra <103060103+PezzuttiFra@users.noreply.github.com>
2022-04-20 11:29:37 +0200

    Add files via upload

---
+++ bubble sort completo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "struct.h"
+
+using namespace std;
+
+void bubble_Sort(anagrafiche arr[], int dim){
+
+   	for(int j=0;j<=dim-1;j++)
+        for(int i=0;i<=dim-1;i++){
+            if(arr[i].idPersona > arr[i+1].idPersona)
+                swap(arr[i].idPersona, arr[i+1].idPersona);
+        }//for
+}
+
+int main(){
+
+    int dim =100;           //metto dim=100 per esempio
+    anagrafiche arr[dim];   //creao array chiamato arr
+
+    bubble_Sort(arr, dim);  //faccio il bubble sort
+}
